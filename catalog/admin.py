971e0173c6e1e7604927e361711d0246920a50c3
nilinswap <nilinswap@gmail.com>
2019-10-31 01:19:08 +0530

    borrower: admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,3 +9,18 @@ admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
 admin.site.register(Language)
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+	list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+	list_filter = ('status', 'due_back')
+	
+	fieldsets = (
+		(None, {
+			'fields': ('book', 'imprint', 'id')
+		}),
+		('Availability', {
+			'fields': ('status', 'due_back', 'borrower')
+		}),
+	)
